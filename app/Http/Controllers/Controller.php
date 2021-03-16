d1140ce2a6f97e8251d106295891cb543a9c9625
AVP <xxi.aisthetes@gmail.com>
2021-03-16 14:10:18 +0200

    added base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,25 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+	/**
+     * Return response.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function sendResponse($resp)
+    {
+		if (isset($resp->message)) {
+            return response()->json([
+    			'message' => $resp->message,
+    		], $resp->code);
+		} elseif (isset($resp->data)) {
+            return response()->json($resp->data, $resp->code);
+        } else {
+            return response()->json([
+    			'status' => 'ok',
+    		], $resp->code);
+
+        }
+    }
 }
