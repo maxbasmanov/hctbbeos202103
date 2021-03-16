c3b552b134338242d61c0c6d14662b93204b04a1
AVP <xxi.aisthetes@gmail.com>
2021-03-16 14:10:29 +0200

    added requests

---
+++ app/Http/Requests/API/EOS/v2/TransactionRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests\API\EOS\v2;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TransactionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get data to be validated from the request.
+     *
+     * @return array
+     */
+    public function validationData()
+    {
+        return $this->json()->all();
+    }
+
+    /**
+     * Get all of the input and files for the request.
+     *
+     * @param  array|mixed|null  $keys
+     * @return array
+     */
+    public function all($keys = null)
+    {
+        if (empty($keys)) {
+
+            return parent::json()->all();
+        }
+
+        return collect(parent::json()->all())->only($keys)->toArray();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'student_id' => 'sometimes|string',
+        ];
+    }
+}
